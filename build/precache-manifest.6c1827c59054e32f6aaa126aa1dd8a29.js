c47fb16a583b06bb1a7103493ff45afacd262ea1
aborisenok <leshaborisenok@gmail.com>
2020-10-17 22:48:28 +0300

    test deploy

---
+++ build/precache-manifest.6c1827c59054e32f6aaa126aa1dd8a29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589f8c185a9fe8cf6df260445f599bf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82732b55c48231b6fcc2",
+    "url": "/static/css/main.cccdd9ca.chunk.css"
+  },
+  {
+    "revision": "16ae442a1163e33450db",
+    "url": "/static/js/2.08a62c2f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.08a62c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82732b55c48231b6fcc2",
+    "url": "/static/js/main.2bbd0989.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
