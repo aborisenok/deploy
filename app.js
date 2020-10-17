342fd4c5582c232c59bbaefc272c2343392b5561
aborisenok <leshaborisenok@gmail.com>
2020-10-17 23:28:17 +0300

    fix port

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const app = express(); // create express app
 
+const PORT = process.env.PORT || 5000;
+
 
 app.use(express.static(path.join(__dirname, "build")));
 
@@ -12,6 +14,6 @@ app.use((req, res, next) => {
   });
 
 // start express server on port 5000
-// app.listen(5000, () => {
-//   console.log("server started on port 5000");
-// });
+app.listen(PORT, () => {
+  console.log("server started on port 5000");
+});
